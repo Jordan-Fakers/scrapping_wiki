4769082904cb5454912403e67e82961e7380dec6
Jordan-Fakers <Jordan.j.talvin@gmail.com>
2021-02-14 18:17:35 +0100

    v1.1

--- nutrix.py
+++ nutrix.py
@@ -17,43 +17,91 @@ def insertInBdd(values):
         database='scraptest'
     )
     mycursor = mybdd.cursor()
-    sql = "INSERT INTO articleTitle (image) values (%s)"
     val = values
+    sql = "INSERT INTO articles (title,image,content,date) values (%s,%s,%s,%s)"
 
-    mycursor.execute(sql,val)
+    mycursor.executemany(sql,val)
     mybdd.commit()
     return str(mycursor) + "nouvelle entree"
+# def recupTitre():
+#     listeTitle = []
+#     listeDate = []
+#     listeContent = []
+#     listeImage = []
 
-def recupTitre():
-    listeTitle = []
-    listeDate = []
-    listeContent = []
-    listeImage = []
+#     li_cards = soup.find_all('li', class_="tile")
 
-    li_cards = soup.find_all('li', class_="tile")
+#     for li in li_cards:
+#         # li_title= li.h2.text
+#         # li_img = li.img
+#         listeTitle.append(li_title)
+#         # lidate = li.find('div',class_='timestamp').text
+#         listeDate.append(lidate)
+#         print(lidate)
+#         listeImage.append(li_img)
+#         print(li_img)
 
-    for li in li_cards:
-        li_title= li.h2.text
-        li_img = li.img
-        listeTitle.append(li_title)
-        lidate = li.find('div',class_='timestamp').text
-        listeDate.append(lidate)
-        print(lidate)
-        listeImage.append(li_img)
-        print(li_img)
+#     for liste in listeTitle:
+#         print(liste)
+#         saveValues= insertInBdd([liste])
+#         print(saveValues)
 
-    for liste in listeTitle:
-        print(liste)
-        saveValues= insertInBdd([liste])
-        print(saveValues)
-
-    for liste2 in listeDate:
-        print(liste2)
-        saveValues2 = insertInBdd([liste2])
-        print(saveValues2)
+#     for liste2 in listeDate:
+#         print(liste2)
+#         saveValues2 = insertInBdd([liste2])
+#         print(saveValues2)
     
-    for liste3 in listeImage:
-        print(liste3)
-        saveValues3 = insertInBdd([liste3])
-        print(liste3)
-recupTitre()
+#     for liste3 in listeImage:
+#         print(liste3)
+#         saveValues3 = insertInBdd([liste3])
+#         print(liste3)
+# recupTitre()
+
+#print('search the latest articles')
+
+def getArticle():
+    url='https://wikileaks.org'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    article_main = soup.find_all('li', class_="tile")
+
+    article_titles = []
+    article_dates = []
+    article_contents = []
+    article_images = []
+    
+
+    for article in article_main:
+        article_title = article.h2.text
+        article_img = article.img.attrs['src']
+        # article_img ="http://wikileaks.com" + article.img.attrs['src']
+        article_date = article.find('div',class_='timestamp').text
+        article_content = article.p.text
+
+        article_titles.append(article_title)
+        # for liste in article_titles:
+        #     insertInBdd([liste])
+        article_dates.append(article_date)
+        # insertInBdd([article_dates])
+        article_contents.append(article_content)
+        # insertInBdd([article_contents])
+        article_images.append(article_img)
+        # insertInBdd([article_images])
+        insertInBdd([article_titles],[article_dates],[article_contents],[article_images])
+
+        print(article_titles)
+        print(article_images)
+        print(article_contents)
+        print(article_dates)
+
+getArticle()
+
+
+# CREATE TABLE articles (
+#   id TINYINT(6) UNSIGNED NOT NULL AUTO_INCREMENT,
+#   title VARCHAR(250) NOT NULL,
+#   image VARCHAR(250) NOT NULL,
+#   content VARCHAR(250) NOT NULL,
+#   date VARCHAR(255) NOT NULL,
+#   PRIMARY KEY (id)
+# );
